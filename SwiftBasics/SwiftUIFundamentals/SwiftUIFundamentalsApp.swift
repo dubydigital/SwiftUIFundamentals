e1b9355adcb311a15987f23c90ae779d0e77098b
dubydigital <dangerouspastimelabs@gmail.com>
2026-07-10 16:35:10 -0500

    Adding Appetizers and SwiftBasics files

---
+++ SwiftBasics/SwiftUIFundamentals/SwiftUIFundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFundamentalsApp.swift
+//  SwiftUIFundamentals
+//
+//  Created by Mark Dubouzet on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
